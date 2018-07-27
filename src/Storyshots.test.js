f055133207db7addbb6943a0fb995c8c88e39558
Kostas Mavropalias <mavropalias@gmail.com>
2018-07-27 06:58:31 +0100

    added image snapshot testing

--- src/Storyshots.test.js
+++ src/Storyshots.test.js
@@ -1,3 +1,11 @@
 import initStoryshots from '@storybook/addon-storyshots';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
+// structural test
 initStoryshots();
+
+// visual test
+initStoryshots({
+  suite: 'Image storyshots',
+  test: imageSnapshot({ storybookUrl: 'http://localhost:9009/' })
+});
